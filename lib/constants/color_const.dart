3ab16fc52d44fb7937c5816d279afd237b51bb4a
AbuJohnElias <abujohnelias2001@gmail.com>
2024-11-11 20:38:59 +0530

    remove uwanted and arranged

--- lib/constants/color_const.dart
+++ lib/constants/color_const.dart
@@ -9,7 +9,12 @@ const Color transparent = Colors.transparent;
 const Color primaryTextColor = Color(0xFF464646);
 const Color secondryTextColor = Color(0xFF565656);
 const Color nonHighlighterColor = Color(0xFF767676);
+const Color onButtonColor = Color(0xFF969696);
+
 const Color highlightPinkColor = Color(0xFFFFC0CB);
 const Color highlightBlueColor = Color(0xFFADD8E6);
+const Color highlightYellowColor = Color.fromARGB(155, 255, 255, 0);
+const Color highlightOrangeColor = Color.fromARGB(225, 255, 166, 0);
+const Color highlightRedColor = Color.fromARGB(176, 255, 0, 0);
 
 
--- lib/views/product_details_screen.dart
+++ lib/views/product_details_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +17,14 @@ import 'product_edit_screen.dart';
 class ProductDetailScreen extends StatelessWidget {
   final int productId;
 
-  ProductDetailScreen({required this.productId});
-
+  ProductDetailScreen({super.key, required this.productId});
+  final List<Color> highlightColors = [
+    highlightPinkColor, 
+    highlightBlueColor, 
+    highlightOrangeColor, 
+    highlightYellowColor, 
+    highlightRedColor, 
+  ];
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -56,98 +64,127 @@ class ProductDetailScreen extends StatelessWidget {
         body: Consumer<ProductDetailProvider>(
           builder: (context, provider, _) {
             if (provider.isLoading) {
-              return Center(child: CircularProgressIndicator());
+              return const Center(child: CircularProgressIndicator());
             }
 
             if (provider.product == null) {
-              return Center(child: Text('Failed to load product details'));
+              return const Center(
+                  child: Text('Failed to load product details'));
             }
 
             final product = provider.product!;
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            return Stack(
               children: [
-                Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: white,
-                  child: CachedNetworkImage(
-                    imageUrl: product.thumbnail,
-                    fit: BoxFit.fitHeight,
-                    placeholder: (context, url) => const Center(
-                      child: Shimmer(
-                        gradient: SweepGradient(
-                          colors: [
-                            primaryTextColor,
-                            nonHighlighterColor,
-                          ],
-                        ),
-                        child: SizedBox(
-                          height: 120,
-                          width: 120,
-                        ),
-                      ),
-                    ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                SingleChildScrollView(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 250,
-                            child: Text(
-                              product.title.toUpperCase(),
-                              style: TextStyleConst.poppinsBold20.copyWith(
-                                color: black,
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        color: white,
+                        child: CachedNetworkImage(
+                          imageUrl: product.thumbnail,
+                          fit: BoxFit.fitHeight,
+                          placeholder: (context, url) => const Center(
+                            child: Shimmer(
+                              gradient: SweepGradient(
+                                colors: [
+                                  primaryTextColor,
+                                  nonHighlighterColor,
+                                ],
+                              ),
+                              child: SizedBox(
+                                height: 120,
+                                width: 120,
                               ),
                             ),
                           ),
-                          Text(
-                            '₹${product.price}',
-                            style: TextStyleConst.poppinsBold20.copyWith(
-                              color: black,
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  width: 250,
+                                  child: Text(
+                                    product.title.toUpperCase(),
+                                    style:
+                                        TextStyleConst.poppinsBold20.copyWith(
+                                      color: black,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  '₹${product.price}',
+                                  style: TextStyleConst.poppinsBold20.copyWith(
+                                    color: black,
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        product.brand,
-                        style: TextStyleConst.poppinsSemiBold15.copyWith(
-                          color: primaryTextColor,
+                            Text(
+                              product.brand,
+                              style: TextStyleConst.poppinsSemiBold15.copyWith(
+                                color: primaryTextColor,
+                              ),
+                            ),
+                            SizedBoxExtension.height(8),
+                            Text(
+                              product.description ?? 'No description available',
+                              style: TextStyleConst.poppinsMedium14.copyWith(
+                                color: secondryTextColor,
+                              ),
+                            ),
+                            SizedBoxExtension.height(32),
+                            Wrap(
+                              spacing: 8.0,
+                              runSpacing: 8.0,
+                              children: [
+                                _detailItems(
+                                  product.category ?? "",
+                                  Icons.category_outlined,
+                                ),
+                                _detailItems(
+                                  product.rating.toString(),
+                                  Icons.star_outline,
+                                ),
+                                _detailItems(
+                                  product.returnPolicy.toString(),
+                                  Icons.undo,
+                                ),
+                                _detailItems(
+                                  "${product.stock}In stock",
+                                  Icons.bar_chart,
+                                ),
+                                _detailItems(
+                                  "${product.dimensions.height.toStringAsFixed(0)}L, ${product.dimensions.width.toStringAsFixed(0)}W, ${product.dimensions.depth.toStringAsFixed(0)}B in cms",
+                                  Icons.crop_square_outlined,
+                                ),
+                                _detailItems(
+                                  product.warrantyInformation,
+                                  Icons.shield_outlined,
+                                ),
+                              ],
+                            )
+                          ],
                         ),
                       ),
-                      SizedBoxExtension.height(8),
-                      Text(
-                        product.description ?? 'No description available',
-                        style: TextStyleConst.poppinsMedium14.copyWith(
-                          color: secondryTextColor,
-                        ),
-                      ),
-                      SizedBoxExtension.height(16),
-                      Wrap(
-                        spacing: 8.0,
-                        runSpacing: 8.0,
-                        children: [
-                          _detailItems(product,Icons.category_outlined),
-                          Text('Return Policy: ${product.returnPolicy}'),
-                          Text(
-                              'Shipping Information: ${product.shippingInformation}'),
-                          Text(
-                              'Rating: ${product.rating?.toStringAsFixed(1) ?? 'N/A'}'),
-                          Text('Stock: ${product.stock ?? 'N/A'}'),
-                        ],
-                      )
                     ],
                   ),
+                ),
+                const Align(
+                  alignment: Alignment.bottomCenter,
+                  child: ReusableButton(),
                 ),
               ],
             );
@@ -157,12 +194,12 @@ class ProductDetailScreen extends StatelessWidget {
     );
   }
 
-  Container _detailItems(Product product,IconData icon) {
+  Container _detailItems(String product, IconData icon) {
     return Container(
       padding: const EdgeInsets.all(10),
-      decoration: const ShapeDecoration(
-        color: highlightBlueColor,
-        shape: SmoothRectangleBorder(
+      decoration:  ShapeDecoration(
+        color: getRandomColor(),
+        shape: const SmoothRectangleBorder(
           borderRadius: SmoothBorderRadius.all(
             SmoothRadius(cornerRadius: 15, cornerSmoothing: 1),
           ),
@@ -171,16 +208,65 @@ class ProductDetailScreen extends StatelessWidget {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-           Icon(
+          Icon(
             icon,
             size: 18,
           ),
           SizedBoxExtension.width(4),
           Text(
-            product.category?.toUpperCase() ?? "".toUpperCase(),
+            product.toUpperCase(),
             style: TextStyleConst.poppinsMedium14,
           ),
         ],
+      ),
+    );
+  }
+
+  Color getRandomColor() {
+    Random random = Random();
+    return highlightColors[random.nextInt(highlightColors.length)];
+  }
+}
+
+class ReusableButton extends StatelessWidget {
+  const ReusableButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 66,
+        margin: const EdgeInsets.all(16),
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        width: double.infinity,
+        decoration: const ShapeDecoration(
+          color: onButtonColor,
+          shape: SmoothRectangleBorder(
+            borderRadius: SmoothBorderRadius.all(
+              SmoothRadius(cornerRadius: 20, cornerSmoothing: 1),
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "ADD TO CART",
+              style: TextStyleConst.poppinsSemiBold14.copyWith(color: white),
+            ),
+            SizedBoxExtension.width(8),
+            const Icon(
+              Icons.arrow_forward_rounded,
+              color: white,
+              size: 20,
+            )
+          ],
+        ),
       ),
     );
   }
